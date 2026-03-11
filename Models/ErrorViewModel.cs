71bd295231e9d07e25f4efcc700bb43076e3ff21
agustindelgado555@gmail.com <agustindelgado555@gmail.com>
2026-03-11 17:07:53 -0300

    Creando la estructura mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinalTaller2_Test_2021.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
